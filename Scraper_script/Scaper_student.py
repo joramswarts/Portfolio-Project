3edca5a316df62d0212bd5f0c69231ff00bb6705
AnnebeldeWaart <annebeldewaart@gmail.com>
2026-03-29 15:22:26 +0200

    Check of zip bestand is gedownload

--- Scraper_script/Scaper_student.py
+++ Scraper_script/Scaper_student.py
@@ -1,19 +1,30 @@
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-download_dir = os.path.join(os.getcwd(), "downloads")
+download_dir = "downloads"
 os.makedirs(download_dir, exist_ok=True)
 
-chrome_options = Options()
-chrome_options.add_experimental_option("prefs", {
-    "download.default_directory": download_dir
+options = Options()
+options.add_experimental_option("prefs", {
+    "download.default_directory": os.path.abspath(download_dir)
 })
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=options)
 
 try:
     driver.get("https://archive.ics.uci.edu/dataset/320/student+performance")
-    # Download click blijft hetzelfde als vorige versie
+
+    # klik op download
+    # ...
+
+    # check op ZIP-bestand
+    time.sleep(5)
+    files = [f for f in os.listdir(download_dir) if f.endswith(".zip")]
+    if files:
+        print("ZIP-bestand gevonden:", files[0])
+    else:
+        print("Geen ZIP-bestand gedownload.")
 finally:
     driver.quit()
