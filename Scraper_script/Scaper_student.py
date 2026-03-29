4ffdd7196bbd172efd88cc038d079af999fd34d4
AnnebeldeWaart <annebeldewaart@gmail.com>
2026-03-29 15:21:52 +0200

    Instellen de map voor het downloaden van scrape script

--- Scraper_script/Scaper_student.py
+++ Scraper_script/Scaper_student.py
@@ -1,22 +1,19 @@
+import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+download_dir = os.path.join(os.getcwd(), "downloads")
+os.makedirs(download_dir, exist_ok=True)
+
+chrome_options = Options()
+chrome_options.add_experimental_option("prefs", {
+    "download.default_directory": download_dir
+})
+
+driver = webdriver.Chrome(options=chrome_options)
 
 try:
     driver.get("https://archive.ics.uci.edu/dataset/320/student+performance")
-
-    try:
-        btn = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH,
-                "/html/body/div/div[1]/div[1]/main/div/div[2]/div[1]/a"))
-        )
-        btn.click()
-        print("Download gestart.")
-    except Exception as e:
-        print("Error bij klikken:", e)
-
+    # Download click blijft hetzelfde als vorige versie
 finally:
     driver.quit()
