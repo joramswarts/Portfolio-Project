a6e3acda5a93a666fd632d7f02b48e94392c1f40
AnnebeldeWaart <annebeldewaart@gmail.com>
2026-03-29 15:23:22 +0200

    Zip bestand uitpakken

--- Scraper_script/Scaper_student.py
+++ Scraper_script/Scaper_student.py
@@ -1,5 +1,6 @@
 import os
 import time
+import zipfile
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -8,7 +9,8 @@ os.makedirs(download_dir, exist_ok=True)
 
 options = Options()
 options.add_experimental_option("prefs", {
-    "download.default_directory": os.path.abspath(download_dir)
+    "download.default_directory": os.path.abspath(download_dir),
+    "download.prompt_for_download": False
 })
 
 driver = webdriver.Chrome(options=options)
@@ -17,14 +19,13 @@ try:
     driver.get("https://archive.ics.uci.edu/dataset/320/student+performance")
 
     # klik op download
-    # ...
 
-    # check op ZIP-bestand
     time.sleep(5)
     files = [f for f in os.listdir(download_dir) if f.endswith(".zip")]
     if files:
-        print("ZIP-bestand gevonden:", files[0])
-    else:
-        print("Geen ZIP-bestand gedownload.")
+        zip_path = os.path.join(download_dir, files[0])
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(download_dir)
+        print("ZIP uitgepakt.")
 finally:
     driver.quit()
