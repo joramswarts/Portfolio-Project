38c566d4a38821e67e1bb7b003cbf39f36b9e144
AnnebeldeWaart <annebeldewaart@gmail.com>
2026-03-29 15:20:37 +0200

    Start scrape download knop vinden

--- Scraper_script/Scaper_student.py
+++ Scraper_script/Scaper_student.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
 try:
     driver.get("https://archive.ics.uci.edu/dataset/320/student+performance")
-    time.sleep(5)
+
+    try:
+        download_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH,
+                "/html/body/div/div[1]/div[1]/main/div/div[2]/div[1]/a"))
+        )
+        print("Downloadknop gevonden.")
+    except:
+        print("Downloadknop niet gevonden.")
+
 finally:
     driver.quit()
